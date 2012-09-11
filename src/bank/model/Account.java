0e14714a29432d60849fc82f133805fbbe398614
Zabell <pierre@zevo.dk>
2012-09-11 11:45:04 +0200

    Basic Models

--- src/bank/model/Account.java
+++ src/bank/model/Account.java
@@ -1,4 +1,21 @@
 package bank.model;
 
-public abstract class Account {
+public class Account {
+	private double balance;
+	
+	public Account() {
+		balance = 0;
+	}
+	
+	public Account(double setBalance) {
+		balance = setBalance;
+	}
+	
+	public void setBalance(double amount) {
+		balance = balance + amount;
+	}
+	
+	public double getBalance() {
+		return balance;
+	}
 }
